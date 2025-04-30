39691c622df80db8f46a31332e40d9deeed90a9d
Chrystina <144075668+chryssstina@users.noreply.github.com>
2025-04-29 23:47:58 -0300

    Update biblio.py
    Mudei de livros para acervo;
    Fiz uma função a parte para verificar se a quantidade não era nula;
    Adicionei a validação para que os códigos sejam únicos;
    Correções gramaticais; 
    Corrigi a função de atualizar;
    Deixei os nomes da funções principais no infinitivo.

--- biblio.py
+++ biblio.py
@@ -1,25 +1,42 @@
-livros = []
-def cadastro():
-    codigo = int(input("Digite o codigo do livro: "))
-    nome = input("Digite o nome do livro: ")
-    autor = input("Digite o nome do autor: ")
-    quant = int(input("Digite a quantidade de livros disponivel: "))
-    if quant < 0:
-        while quant < 0:
-            quant = int(input("Quantidade invalida! Por favor, insira um valor valido."))
+acervo = []
+
+def valor_valido():
+    quant = int(input("Digite a quantidade de livros disponível:\n"))
+    while quant < 0:
+        quant = int(input("Quantidade inválida! Por favor, insira um valor válido:\n"))
+    return quant 
+     
+def cadastrar():
+    while True:
+        codigo = int(input("Digite o código do livro:\n"))
+        codigo_existe = False
+        
+        for livro in acervo:
+            if livro['codigo'] == codigo:
+                print("Já existe um livro cadastrado com este código. Verifique e digite novamente:\n")
+                codigo_existe = True
+                break
+        if not codigo_existe:
+            break
+
+    nome = input("Digite o nome do livro:\n")
+    autor = input("Digite o nome do autor:\n")
+    quant = valor_valido()
+    
 
     livro = {'codigo': codigo,
              'nome': nome,
              'autor': autor,
              'quant': quant}
-    livros.append(livro)
+             
+    acervo.append(livro)
 
     print("Livro cadastrado com sucesso!")
 
-def consulta():
-    codigo = int(input("Informe o codigo do livro para busca: "))
+def consultar():
+    codigo = int(input("Informe o código do livro para busca:\n"))
     encontrado = False
-    for livro in livros:
+    for livro in acervo:
         if livro['codigo'] == codigo:
             print(f"Código: {livro['codigo']}\nNome: {livro['nome']}\nAutor: {livro['autor']}\nQuantidade: {livro['quant']}")
             encontrado = True
@@ -28,22 +45,23 @@ def consulta():
         print("Livro não encontrado.")
 
 def atualizar():
-    codigo = int(input("Informe o codigo do livro pata alterar sua quantidade: "))
-    for i, livro in enumerate(livros):
+    codigo = int(input("Informe o código do livro para alterar sua quantidade:\n"))
+    encontrado = False
+    
+    for livro in acervo:
         if livro['codigo'] == codigo:
-            quantidade = int(input("Digite a nova quantidade: "))
-            while quantidade < 0:
-                    quantidade = int(input("Quantidade invalida! Por favor, insira um valor valido."))
-            livro['quant'] = quantidade
-            print("Estoque atualizado.")
-        else:
-            print("Livro não encontrado.")
+            livro['quant'] = valor_valido()
+            print("Valor atualizado no estoque.")
+            encontrado = True
+            break
+    if not encontrado:
+        print("Livro não encontrado.")
 
 def listar():
-    if not livros:
+    if not acervo:
         print("Nenhum livro cadastrado.")
 
-    for livro in livros:
+    for livro in acervo:
         print(f"Código: {livro['codigo']} | Nome: {livro['nome']} | Autor: {livro['autor']} | Quantidade: {livro['quant']}")
 
 def menu():
@@ -55,20 +73,21 @@ def menu():
         print("3 - Atualizar estoque")
         print("4 - Exibir todos os livros")
         print("0 - Sair")
-        numero = int(input("Escolha uma opção numerica: "))
+        
+        numero = int(input("Escolha uma opção numérica:\n"))
 
         if numero == 1:
-            cadastro()
+            cadastrar()
         elif numero == 2:
-            consulta()
+            consultar()
         elif numero == 3:
             atualizar()
         elif numero == 4:
             listar()
         elif numero == 0:
-            print("Ate mais!")
+            print("Até mais!")
             escolha = False
         else:
-            print("Opção invalida, tente novamente.")
+            print("Opção inválida, tente novamente. \n")
 
 menu()
